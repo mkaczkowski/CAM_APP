d191ed26552de749ea905444dfb02030c19a6b8d
aaadsadad <oro@op.pl>
2014-07-16 12:14:16 +0200

    fsdfsd

--- RANSAGEN_CHIDORI_JUTSU/7___keygen.bat
+++ RANSAGEN_CHIDORI_JUTSU/7___keygen.bat
@@ -1,2 +1,2 @@
 cd /d %~dp0
-keytool -genkey -v -keystore stuff\saiyan_camera.keystore -alias saiyan_camera -keyalg RSA -keysize 2048 -validity 10000
+keytool -genkey -v -keystore stuff\saiyan_camera2.keystore -alias saiyan_camera2 -keyalg RSA -keysize 2048 -validity 10000
