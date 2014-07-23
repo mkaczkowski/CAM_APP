e6a22322f301c9ad7d0af29a6f6e1a68a1cad207
aaadsadad <oro@op.pl>
2014-07-23 15:49:57 +0200

    fsdf

---
+++ SUPER_HERO_SELFIE/stuff/config_test.js
@@ -0,0 +1,19 @@
+'use strict';
+angular.module('sioWebApp.config', [])
+		.constant('configuration', {
+			name : 'Otaku Camera',
+			title : '',
+			buttonBarColor : 'rgba(73, 73, 73, 0.13)',
+			hasPapirus: JSON.parse('false'),
+			titleColor : '#353535',
+			version : '1.0.0',
+            marketUrl: 'com.camapp.otaku',
+			admobBannerKey : '',
+			admobHomeKey : '',
+			admobSaveKey : '',
+			admobShareKey: '',
+			admobBackToHomeKey: '',
+			isProd: JSON.parse('false'),
+			showAds: JSON.parse('false')
+		}
+);
